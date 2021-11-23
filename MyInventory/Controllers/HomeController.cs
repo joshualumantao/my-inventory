9deb4aa61bfa61b7eec75a348cbfb6255338f139
joshuadwight <joshuadwight3@gmail.com>
2021-11-23 15:10:13 +0800

    -m

--- MyInventory/Controllers/HomeController.cs
+++ MyInventory/Controllers/HomeController.cs
@@ -46,6 +46,39 @@ namespace MyInventory.Controllers
         public IActionResult Contact(Contact record)
         {
             Random rnd = new Random();
+            MailMessage mail = new MailMessage()
+            {
+                From = new MailAddress("joshuadwight3@gmail.com", "MyInventory Admin")
+            };
+            mail.To.Add(new MailAddress(record.Email));
+
+            mail.Subject = "Inquiry from "+ record.SenderName + " (" + record.Subject+")";
+            string message = "Hello, sender " + record.SenderName + "<br/><br/><br/>" +
+                "Thank you for contacting our websiite, we appreciate every feedback. <br/>" +
+                "Please see your feedback information below: <br/><br/>" +
+                "Ticket #: <strong>TCT" + rnd.Next(100000, 999999) + "</strong> <br/>" +
+                "Contact Number: <strong>" + record.ContactNo + "</strong> <br/>" +
+                "Sender: <strong>" + record.Email + "</strong><br/>" +
+                "Subject: <strong>" + record.Subject + "</strong><br/>" +
+                "Message: <br/><strong>" + record.Message + "</strong><br/><br/>" +
+                "Please wait for our reply, thank you!";
+
+            mail.Body = message;
+            mail.IsBodyHtml = true;
+
+            using SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("joshuadwight3@gmail.com", "phadz2003"),
+                EnableSsl = true
+
+            };
+            smtp.Send(mail);
+            ViewBag.Message = "Inquiry Sent";
+            return View();
+
+
+
+            /*Random rnd = new Random();
             string message = "Hello, sender " + record.SenderName + "<br/><br/><br/>" +
                 "Thank you for contacting our websiite, we appreciate every feedback. <br/>"+
                 "Please see your feedback information below: <br/><br/>"+
@@ -64,17 +97,16 @@ namespace MyInventory.Controllers
             mail.IsBodyHtml = true;
             mail.Priority = MailPriority.High;
 
-            using SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587) {
+            using SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587) 
+            {
                 Credentials = new NetworkCredential("joshuadwight3@gmail.com", "phadz2003"),
                 EnableSsl = true
             
-            };
+            };*/
 
-            ViewBag.Message = "Inquiry Sent";
 
-            smtp.Send(mail);
 
-            return View();
+
         }
     }
 }
